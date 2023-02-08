42aa6bfd2ece9cab93714fc63617c9602a433f05
HussamAltalib <hussamaltalib@outlook.com>
2023-02-08 13:29:43 +0300

    LAB_ORM_2.5_Relations

---
+++ Blog/main/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+app_name = "main"
+
+urlpatterns = [ #<blog_id> represent object's id changes depends on which blog I clicked | as a hard code we can use it like details/1
+    path("", views.index, name="index_page"),
+    path("posts/add/", views.add_post, name="add_post"),
+    path("light/", views.light, name="light_mode"),
+    path("dark/", views.dark, name="dark_mode"),
+    path("details/<blog_id>/", views.blog_details, name="details_page"),
+    path("update/<blog_id>/", views.update, name="update_page"),
+    path("delete/<blog_id>", views.delete_blog, name="delete_page"),
+    path("search/", views.search, name="search_page"),
+    path("review/add/<blog_id>/", views.add_review, name="add_review"),
+
+
+]
