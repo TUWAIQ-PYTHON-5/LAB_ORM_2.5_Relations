c4d64010f97e9dc1e2c7996052f9fd4d64afbc1f
NjoudAlhazmi <njoudz19@gmail.com>
2023-02-08 19:24:18 +0300

    solved

---
+++ Blog/main/views.py
@@ -0,0 +1,80 @@
+from django.shortcuts import render, redirect
+from django.http import HttpRequest, HttpResponse
+from .models import Post, Comment
+
+# Create your views here.
+
+def index(request : HttpRequest):
+    
+    posts = Post.objects.all()
+    context = {"posts" : posts}
+
+    return render(request, "main/index.html", context)
+
+
+def add_post(request : HttpRequest):
+
+    if request.method == "POST":
+        new_post = Post(title=request.POST["title"], content= request.POST["content"], is_published= request.POST["is_published"])
+        new_post.save()
+
+        return redirect("main:index_page")
+
+    return render(request, "main/add_post.html")
+
+def light(request : HttpRequest):
+    response = redirect("main:index_page")
+    response.set_cookie("mode", "light", max_age=60*60*24*7)
+
+    return response
+
+def dark(request : HttpRequest):
+    response = redirect("main:index_page")
+    response.set_cookie("mode", "dark", max_age=60*60*24*7)
+
+    return response
+
+def blog_details(request : HttpRequest, blog_id):
+    blog = Post.objects.get(id = blog_id)
+    comments = Comment.objects.filter(post=blog)
+
+    return render(request, "main/Blog_details.html", {"blog" : blog, "comments" : comments})
+
+
+def update(request : HttpRequest, blog_id):
+
+    blog = Post.objects.get(id=blog_id)
+    if request.method == "POST":
+        blog.title = request.POST["title"]
+        blog.content = request.POST["content"]
+
+        blog.save()
+        return redirect("main:index_page")
+
+    return render(request, "main/update.html", {"blog" : blog})
+
+
+def delete_blog(request : HttpRequest, blog_id):
+    blog = Post.objects.get(id=blog_id)
+    blog.delete()
+    return redirect("main:index_page")
+
+
+def search(request : HttpRequest):
+    if request.method == "POST":
+        searched = request.POST['searched']
+        print(searched)
+        posts = Post.objects.filter(title__contains = searched)
+        return render(request, "main/search_page.html", {"searched" : searched, "posts" : posts})
+    else :
+        return render(request, "main/search_page.html")
+
+
+def add_comment(request : HttpRequest, blog_id):
+
+    if request.method == "POST":
+        blog = Post.objects.get(id=blog_id)
+        new_comment = Comment(post=blog, name=request.POST["name"], content = request.POST["content"])
+        new_comment.save()
+
+    return redirect("main:details_page", blog_id=blog_id)
